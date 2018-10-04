22ad37ad33efe4b2359c32715f947d01dc2d88f7
github username <github email address>
2018-10-03 22:41:09 -0400

    navigation completed

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,29 @@
 class PostsController < ApplicationController
+
+  def index
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def show
+  end 
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+
+  def post_params
+  end
+
 end
